76fff1076473d7f70d9a3f2773f63d81455bff4e
Ian Morrow <igm5007@gmail.com>
2014-10-16 15:08:17 -0500

    code challenge 1 solution

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
 
     @IBOutlet weak var mailButton: UIButton!
     
@@ -28,13 +29,28 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // code will evaluate when the button is pressed.
+        
+        // make labels visible
         messageLabel.hidden = false
+        nameLabel.hidden = false
+        
+        // change labels text to enter text field text
         messageLabel.text = enterMessageTextField.text
+        nameLabel.text = enterNameTextField.text
+        
+        // change color of labels
         messageLabel.textColor = UIColor.redColor()
+        nameLabel.textColor = UIColor.blueColor()
         
+        // clear text fields
         enterMessageTextField.text = ""
-        enterMessageTextField.resignFirstResponder()
+        enterNameTextField.text = ""
         
+        // hide keyboard
+        enterMessageTextField.resignFirstResponder()
+        enterNameTextField.resignFirstResponder()
+        
+        // change mail button after successful send
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
     }
 
